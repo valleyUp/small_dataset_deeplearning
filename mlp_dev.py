7b07b6268958923610477430b07cda2d144f6428
Glenn <512641455@qq.com>
2023-12-31 19:43:27 +0800

    feat(optuna): add optuna sampler and pruner. fix datamodule

--- mlp_dev.py
+++ mlp_dev.py
@@ -1,10 +1,10 @@
 import os
+from lightning.pytorch.utilities.types import EVAL_DATALOADERS
 import pandas as pd
 import optuna
 import torch
 import lightning.pytorch as pl
-from pytorch_lightning import LightningModule, Trainer
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.preprocessing import StandardScaler
@@ -15,7 +15,7 @@ from torchmetrics.functional import accuracy
 from optuna.integration import PyTorchLightningPruningCallback
 
 # 重构网络结构为 LightningModule
-class LitModel(LightningModule):
+class LitModel(pl.LightningModule):
     def __init__(self, input_size, hidden_size, num_classes, dropout_prob, lr, weight_decay):
         super().__init__()
 
@@ -74,34 +74,19 @@ class LitDataModule(pl.LightningDataModule):
     def setup(self, stage: str) -> None:
         scaler = StandardScaler()
         dat = pd.read_csv("./diabetes.csv")
-        X = torch.tensor(scaler.fit_transform(dat.iloc[:, :-2].to_numpy()), dtype=torch.float32)
+        X = torch.tensor(scaler.fit_transform(dat.iloc[:, :-1].to_numpy()), dtype=torch.float32)
         Y = torch.tensor(dat.iloc[:, -1].to_numpy(), dtype=int)
         self.dataset = TensorDataset(X, Y)
-        self.train_dataset, self.val_dataset = random_split(self.dataset, [600, 168])
+        self.train_dataset, self.val_dataset, self.test_dataset = random_split(self.dataset, [0.6, 0.2, 0.2])
 
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=os.cpu_count())
 
     def val_dataloader(self):
-        return DataLoader(self.val_dataset, batch_size=self.batch_size)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=os.cpu_count())
 
-def load_data(batch_size):
-    # 假设X_train, Y_train, X_val, Y_val是已经准备好的数据张量
-    # X_train, Y_train是包含训练特征和标签的张量
-    # X_val, Y_val是包含验证特征和标签的张量
-    scaler = StandardScaler()
-    dat = pd.read_csv("./diabetes.csv")
-    X = torch.tensor(scaler.fit_transform(dat.iloc[:, :-2].to_numpy()), dtype=torch.float32)
-    Y = torch.tensor(dat.iloc[:, -1].to_numpy(), dtype=int)
-    dataset = TensorDataset(X, Y)
-
-    # 将数据转换为PyTorch数据加载器
-    train_dataset, val_dataset, test_dataset = random_split(dataset, [0.6, 0.2, 0.2])
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
-    val_loader = DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False, num_workers=8)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, num_workers=8)
-
-    return train_loader, val_loader, test_loader
+    def test_dataloader(self) -> EVAL_DATALOADERS:
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=os.cpu_count())
 
 # 定义目标函数，这个函数将被Optuna调用来优化超参数
 def objective(trial):
@@ -113,12 +98,12 @@ def objective(trial):
     batch_size = trial.suggest_int('batch_size', 8, 64)
 
     # 创建数据模块
-    #datamodule = LitDataModule(batch_size=batch_size)
-    trainloader, valloader, testloader = load_data(batch_size)
+    datamodule = LitDataModule(batch_size=batch_size)
+    # trainloader, valloader, testloader = load_data(batch_size)
 
     # 创建模型
     model = LitModel(
-        input_size=7,
+        input_size=8,
         hidden_size=hidden_size,
         num_classes=2,
         dropout_prob=dropout_prob,
@@ -127,21 +112,23 @@ def objective(trial):
     )
 
     # 创建 PyTorch Lightning Trainer
-    trainer = Trainer(
+    trainer = pl.Trainer(
         logger=TensorBoardLogger(save_dir='./'),
         enable_progress_bar=False,
         enable_model_summary=False,
         max_epochs=300,
         callbacks=[
-            #PyTorchLightningPruningCallback(trial, monitor="val_loss"),  # 将 Optuna 子试验与 PyTorch Lightning callback 结合
+            PyTorchLightningPruningCallback(trial, monitor="val_loss"),  # 将 Optuna 子试验与 PyTorch Lightning callback 结合
             EarlyStopping(monitor='val_loss', patience=20, mode='min'),
             ModelCheckpoint(monitor='val_loss', mode='min')
         ]
     )
 
     # 训练
-    trainer.fit(model, train_dataloaders=trainloader, val_dataloaders=valloader)
-    trainer.test(model, testloader, 'best')
+    #trainer.fit(model, train_dataloaders=trainloader, val_dataloaders=valloader)
+    #trainer.test(model, testloader, 'best')
+    trainer.fit(model=model, datamodule=datamodule)
+    trainer.test(model=model, datamodule=datamodule, ckpt_path='best')
 
     # 获取验证集上的损失
     val_loss = trainer.callback_metrics['test_acc'].item()
@@ -149,8 +136,12 @@ def objective(trial):
     return val_loss
 
 # 创建一个 Optuna 研究（Study），并找到最佳超参数
-study = optuna.create_study(direction='maximize')
-study.optimize(objective, n_trials=50)
+study = optuna.create_study(
+    direction='maximize',
+    sampler=optuna.samplers.TPESampler(),
+    pruner=optuna.pruners.MedianPruner(n_startup_trials=5, n_warmup_steps=30),
+)
+study.optimize(objective, n_trials=30)
 
 print('Number of finished trials:', len(study.trials))
 print('Best trial:')
